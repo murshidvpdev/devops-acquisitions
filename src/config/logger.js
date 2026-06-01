5997efd7d3e7824837d38bf92253b3c9626ac39d
murshi . <murshi.@murshis-MacBook-Air.local>
2026-06-01 16:25:00 +0530

    added

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,4 +1,4 @@
-import winston from 'winston';
+import winston  from'winston';
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
@@ -6,28 +6,22 @@ const logger = winston.createLogger({
     winston.format.timestamp(),
     winston.format.errors({stack:true}),
     winston.format.json()
-  )),
-  defaultMeta: { service: 'user-service' },
+  ),
+  defaultMeta: { service: 'acquisition-api' },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or higher to `error.log`
-    //   (i.e., error, fatal, but not other levels)
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    //
-    // - Write all logs with importance level of `info` or higher to `combined.log`
-    //   (i.e., fatal, error, warn, and info, but not trace)
-    //
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
+
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple(),
+    ),
   }));
 }
+
+export default logger;
