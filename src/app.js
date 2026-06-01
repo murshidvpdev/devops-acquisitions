7d55ad68fc246975343f22eb3bd7f97d568b1737
murshi . <murshi.@murshis-MacBook-Air.local>
2026-06-01 12:11:50 +0530

    eslint linked

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-import express from "express"
+import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-    res.status(200).send("Hello from Acquestions!!")
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Acquestions!!');
+});
 
 export default app;
